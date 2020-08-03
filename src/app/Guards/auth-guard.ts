3cea17bf6f5ff8f8bba2dfc547b540e8750d2400
Joan Titin <joanmanueltitin@gmail.com>
2020-08-02 22:42:49 -0400

    Resolve titin Tasks

--- src/app/Guards/auth-guard.ts
+++ src/app/Guards/auth-guard.ts
@@ -3,9 +3,7 @@ import { CanActivate, Router } from '@angular/router';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router) { }
 
   canActivate(): boolean {
 
